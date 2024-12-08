7f802e316db013095cf3515034617980aaa017eb
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-08 18:39:53 +0530

    Add html template and fix eslint cjs error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
 ];
